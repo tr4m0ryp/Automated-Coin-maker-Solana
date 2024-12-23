8ce33f073c2d39a9752f3c48d9c9905909638304
tr4m0ryp - AppelMoussa <tramoryp@proton.me>
2024-12-23 14:12:43 +0100

    Create logger.ts

---
+++ src/utils/logger.ts
@@ -0,0 +1,15 @@
+import chalk from "chalk";
+export const logger = {
+  info: (message: string) => {
+    console.log(chalk.blue("[INFO]"), message);
+  },
+  success: (message: string) => {
+    console.log(chalk.green("[SUCCESS]"), message);
+  },
+  warning: (message: string) => {
+    console.log(chalk.yellow("[WARNING]"), message);
+  },
+  error: (message: string) => {
+    console.error(chalk.red("[ERROR]"), message);
+  },
+};
