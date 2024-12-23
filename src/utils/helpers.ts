3d9906d94e03112d8c19f296104cd1dcf6233ec4
tr4m0ryp - AppelMoussa <tramoryp@proton.me>
2024-12-23 14:27:19 +0100

    Update helpers.ts

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,8 +1,5 @@
-import { CONFIG } from "../config";
 import { logger } from "./logger";
 
 export function validateEnvVariables(): void {
-  const { tokenDetails } = CONFIG;
-
   logger.info("Environment variables validated successfully.");
 }
